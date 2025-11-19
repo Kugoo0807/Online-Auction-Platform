e653ce5dd7f0f4596ba940659687a0cc6a6c6a57
huiw14 <fancy14042005@gmail.com>
2025-11-20 02:30:55 +0700

    fix(auction): sửa logic tìm sp theo ID

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,36 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import AuctionDetail from './pages/AuctionDetail'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/auction/:id" element={<AuctionDetail />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+
+          {/* Protected route */}
+          <Route
+            path="/auction/:id"
+            element={
+              <ProtectedRoute>
+                <AuctionDetail />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
