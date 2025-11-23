5498dc8149994ae742374361b3f629d8d1ea1eff
Kugoo <khoanqd0807@gmail.com>
2025-11-24 02:31:13 +0700

    fix(database): Sửa lại schema và seed db

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const DB_URI = process.env.MONGODB_URI || "mongodb+srv://Web:1111@database.b0lr6f1.mongodb.net/onlineauction?retryWrites=true&w=majority";
 
 const connectDB = async () => {
-  try {
-    const connOptions = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
+  if (!DB_URI) {
+    console.error("[MONGOOSE] Lỗi: Không tìm thấy biến môi trường MONGODB_URI trong file .env");
+    process.exit(1);
+  }
 
-    await mongoose.connect(DB_URI, connOptions);
-    console.log(`Kết nối MongoDB Atlas thành công!`);
+  try {
+    await mongoose.connect(DB_URI);
+
+    console.log(`[MONGOOSE] Kết nối MongoDB Atlas thành công!`);
+    console.log(`[MONGOOSE] Host: ${mongoose.connection.host}`);
 
   } catch (err) {
-    console.error(`Kết nối MongoDB thất bại: ${err.message}`);
+    console.error(`[MONGOOSE] Kết nối MongoDB thất bại: ${err.message}`);
     process.exit(1);
   }
 };
