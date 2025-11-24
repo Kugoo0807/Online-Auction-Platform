5eb9eb256a198340256fbbe47313bd3ed057a3cf
huiw14 <fancy14042005@gmail.com>
2025-11-24 20:53:58 +0700

    feat: Auth + Routing

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,18 +1,11 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-<<<<<<< HEAD
 export default function ProtectedRoute({ children, allowedRoles }) {
   const { user, loading } = useAuth();
   const location = useLocation();
 
   if (loading) return <div>Loading...</div>;
-=======
-export default function ProtectedRoute({ children }) {
-  const { user } = useAuth();
-  const location = useLocation();
-
->>>>>>> d8a75a7afe93f1808d1dcd0b2e71809e6dad6131
   // kiểm tra nếu người dùng chưa đăng nhập
   if (!user) {
     // chuyển hướng đến trang đăng nhập
@@ -20,14 +13,11 @@ export default function ProtectedRoute({ children }) {
     return <Navigate to="/login" replace state={{ from: location.pathname }} />;
   }
 
-<<<<<<< HEAD
   // đăng nhập nhưng sai role -> đẩy về home
   if (allowedRoles && !allowedRoles.includes(user.role)) {
     alert("Bạn không có quyền truy cập trang này.");
     return <Navigate to ="/" replace />;
   }
 
-=======
->>>>>>> d8a75a7afe93f1808d1dcd0b2e71809e6dad6131
   return children ? children : <Outlet />;
 }
