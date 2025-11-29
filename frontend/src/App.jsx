e27c94b4fdeadb78bf694c2a6ea0379b89a2383b
ngnam2012 <154614766+ngnam2012@users.noreply.github.com>
2025-11-29 13:22:02 +0700

    feature(frontend): tính năng search (#45)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,46 @@
-// src/App.jsx
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import MainLayout from './components/layout/MainLayout'
 
-// Import các trang
+// Pages
 import HomePages from './pages/HomePages'
 import Login from './pages/Login'
-import Signup from './pages/Signup' // Lưu ý tên file bạn đặt là SignUp hay Signup nhé
+import SignUp from './pages/SignUp' 
 import Dashboard from './pages/Dashboard'
 import AuctionDetail from './pages/AuctionDetail'
 import OAuthCallback from './pages/OAuthCallback'
 import CategoryPage from './pages/CategoryPage';
-// Import các Route bảo vệ (nếu bạn đã tạo file component này rồi)
-import ProtectedRoute from './components/ProtectedRoute'
-import GuestRoute from './components/GuestRoute'
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
+import SearchPage from './pages/SearchPage'; // Trang tìm kiếm
+
+// Components
+import ProtectedRoute from './components/ProtectedRoute'
+import GuestRoute from './components/GuestRoute'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        
-        {/* 1. Các trang Public (Ai cũng vào được) */}
+        {/* Public */}
         <Route path="/" element={<HomePages />} />
         <Route path="/auction/:id" element={<AuctionDetail />} />
         <Route path="/category/:slug" element={<CategoryPage />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="/oauth/callback" element={<OAuthCallback />} />
         
-        
-        {/* 2. Các trang Khách (Đã đăng nhập thì không vào được nữa) */}
+        {/* Guest only */}
         <Route element={<GuestRoute />}>
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
         </Route>
 
-        {/* 3. Các trang Private (Phải đăng nhập mới vào được) */}
+        {/* Private */}
         <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route path="/create-auction" element={<CreateAuction />} /> */}
         </Route>
-
       </Route>
     </Routes>
   )
