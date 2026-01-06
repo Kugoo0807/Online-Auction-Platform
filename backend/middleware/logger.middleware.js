26eec3055402af45e7b762808d3a1f006664864b
Nguyen Quang Dang Khoa <khoanqd0807@gmail.com>
2026-01-06 10:21:57 +0700

    fix: Auto refresh token sau 13 phút

--- backend/middleware/logger.middleware.js
+++ backend/middleware/logger.middleware.js
@@ -1,5 +1,15 @@
 const simpleLogger = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+    res.on('finish', () => {
+        const status = res.statusCode;
+        let color = '\x1b[0m'; // default
+        
+        if (status >= 200 && status < 300) color = '\x1b[32m'; // green
+        else if (status >= 300 && status < 400) color = '\x1b[36m'; // cyan
+        else if (status >= 400 && status < 500) color = '\x1b[33m'; // yellow
+        else if (status >= 500) color = '\x1b[31m'; // red
+        
+        console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl} - ${color}${status}\x1b[0m`);
+    });
     
     next();
 };
